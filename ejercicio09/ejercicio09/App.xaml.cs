e7c4f8cc1d9a0959281df62415df0237734600dc
ANTONIOS-MAJ-HE <antonio.santiago@tecsup.edu.pe>
2023-10-21 20:33:33 -0500

    terminado tarea

--- ejercicio09/ejercicio09/App.xaml.cs
+++ ejercicio09/ejercicio09/App.xaml.cs
@@ -10,7 +10,7 @@ namespace ejercicio09
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
--- ejercicio09/ejercicio09/MainPage.xaml.cs
+++ ejercicio09/ejercicio09/MainPage.xaml.cs
@@ -13,6 +13,38 @@ namespace ejercicio09
         public MainPage()
         {
             InitializeComponent();
+            tareacss.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new tareacss());
+            };
+            ExplicitC.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplicitControlDemo());
+            };
+            Explicit.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplicitDemo());
+            };
+            ImplicitC.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ImplicitControlDemo());
+            };
+            Implicit.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ImplicitDemo());
+            };
+            Global.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new globaldemo());
+            };
+            Inherente.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new InheritanceDemo());
+            };
+            Dynamic.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new DynamicDemo());
+            };
         }
     }
 }
